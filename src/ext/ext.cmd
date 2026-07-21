414a0f5854e11fd6c15726d41566a9a7dfb1d8ef
mtaas-v0 <zliu.dayjob@gmail.com>
2026-07-21 18:54:44 -0400

    Update ext.cmd

--- src/ext/ext.cmd
+++ src/ext/ext.cmd
@@ -10,7 +10,7 @@
 @if /i "%1"=="inc" set _SuppressWixClean=true
 @if not "%1"=="" shift & goto parse_args
 
-msbuild ext_t.proj -p:AcceptEula=true -p:Configuration=%_C% -p:SuppressWixClean=%_SuppressWixClean% -m -tl -nologo -warnaserror -bl:%_L%\ext_build.binlog || exit /b
+msbuild ext_t.proj -p:AcceptEula=wix8 -p:Configuration=%_C% -p:SuppressWixClean=%_SuppressWixClean% -m -tl -nologo -warnaserror -bl:%_L%\ext_build.binlog || exit /b
 
 @popd
 @endlocal
