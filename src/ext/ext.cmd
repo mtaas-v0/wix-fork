7336caa04d371b76b5acbf0dda511bf73b75350e
mtaas-v0 <zliu.dayjob@gmail.com>
2026-07-21 21:14:11 -0400

    Update ext.cmd

--- src/ext/ext.cmd
+++ src/ext/ext.cmd
@@ -11,6 +11,11 @@
 @if not "%1"=="" shift & goto parse_args
 
 wix eula accept
+echo "[DEBUG] ext.cmd here"
+
+if not exist "%USERPROFILE%\.wix" mkdir ""%USERPROFILE%\.wix" 
+
+echo "PLACEHOLDER" >>  "%USERPROFILE%\.wix\wix8-osmf-eula.txt"
 
 msbuild ext_t.proj -p:AcceptEula=wix8 -p:Configuration=%_C% -p:SuppressWixClean=%_SuppressWixClean% -m -tl -nologo -warnaserror -bl:%_L%\ext_build.binlog || exit /b
 
