0ac6fa55bc2400ecac30c19ac32b9d43a9c4642d
mtaas-v0 <zliu.dayjob@gmail.com>
2026-07-21 23:02:37 -0400

    Update test.cmd
    [no ci]

--- src/test/test.cmd
+++ src/test/test.cmd
@@ -11,9 +11,17 @@
 @if not "%RuntimeTestsEnabled%"=="true" echo Build integration tests %_C%
 @if "%RuntimeTestsEnabled%"=="true" set _T=test&echo Run integration tests %_C%
 
+echo "::group::test_burn.cmd"
 @call burn\test_burn.cmd %_C% %_T% || exit /b
+echo "::endgroup::"
+
+echo "::group::test_msi.cmd"
 @call msi\test_msi.cmd %_C% %_T% || exit /b
+echo "::endgroup::"
+
+echo "::group::test_wix.cmd"
 @call wix\test_wix.cmd %_C% %_T% || exit /b
+echo "::endgroup::"
 
 @popd
 @endlocal
