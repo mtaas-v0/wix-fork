211bb1febe13df1f68b4bdd5201c6f52cd421a7f
mtaas-v0 <zliu.dayjob@gmail.com>
2026-07-21 22:16:40 -0400

    Update build_init.cmd

--- src/build_init.cmd
+++ src/build_init.cmd
@@ -7,19 +7,19 @@ md ..\build\artifacts
 md ..\build\logs\crashdumps
 md ..\build\logs\TestResults
 
-msbuild -Restore internal\SetBuildNumber\SomeVerInit.verproj -nologo -p:SomeVerMinimumMajorMinor=8.0
+msbuild -Restore internal\SetBuildNumber\SomeVerInit.verproj -nologo -p:SomeVerMinimumMajorMinor=8.0  -bl:%_L%\somever_init.binlog
 
 echo ":: LIST TARGETS"
-msbuild internal\SetBuildNumber\SomeVerInit.verproj -targets
+:: msbuild internal\SetBuildNumber\SomeVerInit.verproj -targets
 
-echo ":: BEFORE msbuild SomeVerInit.verproj"
-msbuild internal\SetBuildNumber\SomeVerInit.verproj -target:SomeVerApplyVersionTemplates -bl:%_L%\somever_init.binlog
-echo ":: END msbuild SomeVerInit.verproj"
+:: echo ":: BEFORE msbuild SomeVerInit.verproj"
+:: msbuild internal\SetBuildNumber\SomeVerInit.verproj -target:SomeVerApplyVersionTemplates
+:: echo ":: END msbuild SomeVerInit.verproj"
 :: msbuild -Restore internal\SetBuildNumber\SomeVerInit.verproj -nologo
 
 echo "D:\a\wix-fork\wix-fork\build\SomeVerInfo.cs"
 type  D:\a\wix-fork\wix-fork\build\SomeVerInfo.cs
-exit 1
+:: exit 1
 
 
 
